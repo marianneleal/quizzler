50ce1dbc5203f0d5a6db5db9e100ba44ff2c4c1c
marianneleal <marianneleal93@gmail.com>
2021-11-20 12:23:40 +0100

    modularisierung

--- lib/question.dart
+++ lib/question.dart
@@ -2,7 +2,7 @@ class Question {
   String questionText = 'a';
   bool questionAnswer = true;
 
-  Question({required String q, required bool a}) {
+  Question(String q, bool a) {
     questionText = q;
     questionAnswer = a;
   }
